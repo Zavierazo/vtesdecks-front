ff888d5ad7f5629ff0924071e9cd1e9f56fe06fe
Zavierazo <zavierazo@gmail.com>
2024-09-16 20:56:29 +0200

    Fix some transations & translate dates

--- src/app/modules/deck-card/deck-card.module.ts
+++ src/app/modules/deck-card/deck-card.module.ts
@@ -1,13 +1,22 @@
-import { DeckCardComponent } from './deck-card.component'
-import { NgModule } from '@angular/core'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { TranslocoModule } from '@ngneat/transloco'
+import { TranslocoLocaleModule } from '@ngneat/transloco-locale'
 import { SharedModule } from '../../shared/shared.module'
+import { DeckCardComponent } from './deck-card.component'
 
 @NgModule({
   declarations: [DeckCardComponent],
-  imports: [CommonModule, NgbModule, SharedModule, RouterModule],
+  imports: [
+    CommonModule,
+    NgbModule,
+    SharedModule,
+    RouterModule,
+    TranslocoModule,
+    TranslocoLocaleModule,
+  ],
   exports: [DeckCardComponent],
 })
 export class DeckCardModule {}
