cf9e9a35ba0db4ec405b90b09d54071fb063477f
Zavierazo <zavierazo@gmail.com>
2024-09-19 16:41:17 +0200

    Traduccion tipos de carta

--- src/app/modules/deck-shared/deck-shared.module.ts
+++ src/app/modules/deck-shared/deck-shared.module.ts
@@ -1,19 +1,20 @@
-import { SetTooltipComponent } from './set-tooltip/set-tooltip.component'
-import { LibraryCardComponent } from './library-card/library-card.component'
-import { CryptCardComponent } from './crypt-card/crypt-card.component'
-import { ClanFilterComponent } from './clan-filter/clan-filter.component'
-import { LibraryListComponent } from './library-list/library-list.component'
-import { LibraryComponent } from './library/library.component'
-import { CryptComponent } from './crypt/crypt.component'
-import { NgModule } from '@angular/core'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { CommonModule } from '@angular/common'
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader'
-import { DisciplineFilterComponent } from './discipline-filter/discipline-filter.component'
-import { SharedModule } from '../../shared/shared.module'
+import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
-import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { TranslocoModule } from '@ngneat/transloco'
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics'
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader'
+import { SharedModule } from '../../shared/shared.module'
+import { ClanFilterComponent } from './clan-filter/clan-filter.component'
+import { CryptCardComponent } from './crypt-card/crypt-card.component'
+import { CryptComponent } from './crypt/crypt.component'
+import { DisciplineFilterComponent } from './discipline-filter/discipline-filter.component'
+import { LibraryCardComponent } from './library-card/library-card.component'
+import { LibraryListComponent } from './library-list/library-list.component'
+import { LibraryTypeTranslocoPipe } from './library-type-translation/library-type-transloco.pipe'
+import { LibraryComponent } from './library/library.component'
+import { SetTooltipComponent } from './set-tooltip/set-tooltip.component'
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { TranslocoModule } from '@ngneat/transloco'
     DisciplineFilterComponent,
     ClanFilterComponent,
     SetTooltipComponent,
+    LibraryTypeTranslocoPipe,
   ],
   imports: [
     CommonModule,
--- src/app/utils/library-types.ts
+++ src/app/utils/library-types.ts
@@ -1,24 +1,26 @@
 export class LibraryType {
   name: string
   icon: string
+  label: string
 
-  constructor(name: string, icon: string) {
+  constructor(name: string, icon: string, label: string) {
     this.name = name
     this.icon = icon
+    this.label = label
   }
 }
 
 export const LIBRARY_TYPE_LIST = [
-  new LibraryType('Master', 'master'),
-  new LibraryType('Action', 'action'),
-  new LibraryType('Action Modifier', 'modifier'),
-  new LibraryType('Political Action', 'political'),
-  new LibraryType('Equipment', 'equipment'),
-  new LibraryType('Retainer', 'retainer'),
-  new LibraryType('Ally', 'ally'),
-  new LibraryType('Combat', 'combat'),
-  new LibraryType('Reaction', 'reaction'),
-  new LibraryType('Event', 'event'),
-  new LibraryType('Power', 'power'),
-  new LibraryType('Conviction', 'conviction'),
+  new LibraryType('Master', 'master', 'vtes.type.master'),
+  new LibraryType('Action', 'action', 'vtes.type.action'),
+  new LibraryType('Action Modifier', 'modifier', 'vtes.type.modifier'),
+  new LibraryType('Political Action', 'political', 'vtes.type.political'),
+  new LibraryType('Equipment', 'equipment', 'vtes.type.equipment'),
+  new LibraryType('Retainer', 'retainer', 'vtes.type.retainer'),
+  new LibraryType('Ally', 'ally', 'vtes.type.ally'),
+  new LibraryType('Combat', 'combat', 'vtes.type.combat'),
+  new LibraryType('Reaction', 'reaction', 'vtes.type.reaction'),
+  new LibraryType('Event', 'event', 'vtes.type.event'),
+  new LibraryType('Power', 'power', 'vtes.type.power'),
+  new LibraryType('Conviction', 'conviction', 'vtes.type.conviction'),
 ]
