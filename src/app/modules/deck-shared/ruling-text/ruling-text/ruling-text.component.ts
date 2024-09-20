c1a9a53c6339ac99be65392402a70d965c0ea799
Zavierazo <zavierazo@gmail.com>
2024-09-20 16:50:54 +0200

    Fix sonar

--- src/app/modules/deck-shared/ruling-text/ruling-text/ruling-text.component.ts
+++ src/app/modules/deck-shared/ruling-text/ruling-text/ruling-text.component.ts
@@ -27,65 +27,66 @@ export class RulingTextComponent implements OnInit {
     let currentPart = ''
     let currentType: RulingText['type'] | null = null
     const text = this.ruling.text
-    for (let i = 0; i < text.length; i++) {
-      const char = text[i]
+    for (const char of text) {
       if (currentType === null) {
         if (char === '{') {
           if (currentPart.length > 0) {
             this.rulingsText.push({ type: 'string', text: currentPart })
-            currentPart = ''
           }
           currentPart = ''
           currentType = 'card'
         } else if (char === '[') {
           if (currentPart.length > 0) {
             this.rulingsText.push({ type: 'string', text: currentPart })
-            currentPart = ''
           }
           currentPart = ''
           currentType = 'reference'
         } else {
           currentPart += char
         }
+      } else if (currentType === 'card' && char === '}') {
+        this.addCardText(currentPart)
+        currentPart = ''
+        currentType = null
+      } else if (currentType === 'reference' && char === ']') {
+        this.addReferenceText(currentPart)
+        currentPart = ''
+        currentType = null
       } else {
-        if (currentType === 'card' && char === '}') {
-          const card = this.ruling.cards.find(
-            (card) => card.name === currentPart,
-          )
-          if (card) {
-            this.rulingsText.push({
-              type: 'card',
-              text: currentPart,
-              popoverImage: `/assets/img/cards/${card.id}.jpg`,
-            })
-          } else {
-            this.rulingsText.push({ type: 'card', text: currentPart })
-          }
-          currentPart = ''
-          currentType = null
-        } else if (currentType === 'reference' && char === ']') {
-          const reference = this.ruling.references.find(
-            (ref) => ref.label === currentPart,
-          )
-          if (reference) {
-            this.rulingsText.push({
-              type: 'reference',
-              text: currentPart,
-              link: reference.url,
-            })
-          } else {
-            this.rulingsText.push({ type: 'reference', text: currentPart })
-          }
-          currentPart = ''
-          currentType = null
-        } else {
-          currentPart += char
-        }
+        currentPart += char
       }
     }
     // Add any remaining text outside of formatted parts
     if (currentPart.length > 0) {
       this.rulingsText.push({ type: 'string', text: currentPart })
+    }
+  }
+
+  private addCardText(currentPart: string) {
+    const card = this.ruling.cards.find((card) => card.name === currentPart)
+    if (card) {
+      this.rulingsText.push({
+        type: 'card',
+        text: currentPart,
+        popoverImage: `/assets/img/cards/${card.id}.jpg`,
+      })
+    } else {
+      this.rulingsText.push({ type: 'card', text: currentPart })
+    }
+  }
+
+  private addReferenceText(currentPart: string) {
+    const reference = this.ruling.references.find(
+      (ref) => ref.label === currentPart,
+    )
+    if (reference) {
+      this.rulingsText.push({
+        type: 'reference',
+        text: currentPart,
+        link: reference.url,
+      })
+    } else {
+      this.rulingsText.push({ type: 'reference', text: currentPart })
     }
   }
 }
