0683066a080c0fbdbbc5eeb9625ddc3ae91c13d2
Zavierazo <zavierazo@gmail.com>
2025-04-13 19:09:19 +0200

    Refactor auth store from akita to signals

--- src/app/state/auth/auth.store.ts
+++ src/app/state/auth/auth.store.ts
@@ -1,25 +1,30 @@
-import { Injectable } from '@angular/core'
-import { Store, StoreConfig } from '@datorama/akita'
+import { Injectable, signal } from '@angular/core'
+import { toObservable } from '@angular/core/rxjs-interop'
+import { NgcCookieConsentService } from 'ngx-cookieconsent'
+import { map, Observable } from 'rxjs'
 import { ApiUser } from '../../models/api-user'
 import { LocalStorageService } from '../../services/local-storage.service'
 import { SessionStorageService } from '../../services/session-storage.service'
-import { NgcCookieConsentService } from 'ngx-cookieconsent'
 
 const initialState: ApiUser = {}
 
 @Injectable({
   providedIn: 'root',
 })
-@StoreConfig({ name: AuthStore.storeName })
-export class AuthStore extends Store<ApiUser> {
+export class AuthStore {
   static readonly storeName = 'auth'
+  private readonly state = signal<ApiUser>(initialState)
+  private readonly state$ = toObservable(this.state)
+  private readonly loading = signal<boolean>(false)
+  private readonly loading$ = toObservable(this.loading)
+  private readonly error = signal<string | null | undefined>(null)
+  private readonly error$ = toObservable(this.error)
 
   constructor(
     private localStorage: LocalStorageService,
     private sessionStorage: SessionStorageService,
     private cookieConsentService: NgcCookieConsentService,
   ) {
-    super(initialState)
     const previousState = this.sessionStorage.getValue<ApiUser>(
       AuthStore.storeName,
     )
@@ -65,5 +70,37 @@ export class AuthStore extends Store<ApiUser> {
       this.localStorage.setValue(AuthStore.storeName, this.getValue())
     }
     this.sessionStorage.setValue(AuthStore.storeName, this.getValue())
+  }
+
+  select(selector: (state: ApiUser) => any): Observable<any> {
+    return this.state$.pipe(map(selector))
+  }
+
+  selectLoading(): Observable<boolean> {
+    return this.loading$
+  }
+
+  getValue(): ApiUser {
+    return this.state()
+  }
+
+  getLoading(): boolean {
+    return this.loading()
+  }
+
+  getError(): string | null | undefined {
+    return this.error()
+  }
+
+  update(value: ApiUser) {
+    this.state.update(() => value)
+  }
+
+  setLoading(value: boolean = false) {
+    this.loading.update(() => value)
+  }
+
+  setError(value: string | null | undefined) {
+    this.error.update(() => value)
   }
 }
