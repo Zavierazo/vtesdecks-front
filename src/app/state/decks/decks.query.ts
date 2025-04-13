6214a3dab291f9cc792c62937c5480d3f88ea499
Zavierazo <zavierazo@gmail.com>
2025-04-13 20:00:39 +0200

    Refactor decks store from akita to signals

--- src/app/state/decks/decks.query.ts
+++ src/app/state/decks/decks.query.ts
@@ -1,38 +1,43 @@
 import { Injectable } from '@angular/core'
 import { Params } from '@angular/router'
-import { QueryEntity } from '@datorama/akita'
 import { Observable } from 'rxjs'
 import { ApiDeck } from './../../models/api-deck'
 import { DecksState, DecksStore } from './decks.store'
 @Injectable({
   providedIn: 'root',
 })
-export class DecksQuery extends QueryEntity<DecksState, ApiDeck> {
-  constructor(protected override store: DecksStore) {
-    super(store)
+export class DecksQuery {
+  constructor(private readonly store: DecksStore) {}
+
+  selectLoading(): Observable<boolean> {
+    return this.store.selectLoading()
+  }
+
+  selectAll(): Observable<ApiDeck[]> {
+    return this.store.selectEntities()
   }
 
   selectTotal(): Observable<number> {
-    return this.select((decks: DecksState) => decks.total)
+    return this.store.select((decks: DecksState) => decks.total)
   }
 
   selectParams(): Observable<Params> {
-    return this.select((decks: DecksState) => decks.params)
+    return this.store.select((decks: DecksState) => decks.params)
   }
 
   selectRestorableDecks(): Observable<ApiDeck[]> {
-    return this.select((decks: DecksState) => decks.restorableDecks)
+    return this.store.select((decks: DecksState) => decks.restorableDecks)
   }
 
   getParam(key: string): any {
-    return this.getValue().params[key]
+    return this.store.getValue().params[key]
   }
 
   getHasMore(): boolean {
-    return this.getValue().hasMore
+    return this.store.getValue().hasMore
   }
 
   getOffset(): number {
-    return this.getValue().offset
+    return this.store.getValue().offset
   }
 }
