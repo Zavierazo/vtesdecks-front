244af384b27fc6bee564d2da4ac7b4529ef51f9e
Zavierazo <zavierazo@gmail.com>
2025-04-18 20:26:51 +0200

    Add VTES AI Chat (currently in beta testing)

---
+++ src/app/modules/vtes-ai/vtes-ai.component.ts
@@ -0,0 +1,103 @@
+import { AsyncPipe } from '@angular/common'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  TemplateRef,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core'
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
+import { TranslocoDirective } from '@jsverse/transloco'
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap'
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
+import { MarkdownPipe } from 'ngx-markdown'
+import { MediaService } from '../../services/media.service'
+import { LoadingComponent } from '../../shared/components/loading/loading.component'
+import { VtesAiQuery } from '../../state/vtes-ai/vtes-ai.query'
+import { VtesAiService } from '../../state/vtes-ai/vtes-ai.service'
+
+@UntilDestroy()
+@Component({
+  selector: 'app-vtes-ai',
+  templateUrl: './vtes-ai.component.html',
+  styleUrls: ['./vtes-ai.component.scss'],
+  imports: [
+    TranslocoDirective,
+    ReactiveFormsModule,
+    AsyncPipe,
+    LoadingComponent,
+    MarkdownPipe,
+  ],
+})
+export class VtesAiComponent implements OnInit, AfterViewInit {
+  chats$ = this.query.selectEntities()
+  activeChat$ = this.query.selectActiveChat()
+  loading$ = this.query.selectLoading()
+  isMobileOrTablet$ = this.mediaService.observeMobileOrTablet()
+
+  chatForm = new FormGroup({
+    question: new FormControl('', Validators.minLength(3)),
+  })
+
+  @ViewChild('scrollFrame', { static: false }) scrollFrame!: ElementRef
+  @ViewChildren('item') itemElements!: QueryList<any>
+  private scrollContainer: any
+
+  constructor(
+    private readonly service: VtesAiService,
+    private readonly query: VtesAiQuery,
+    private readonly offcanvasService: NgbOffcanvas,
+    private readonly mediaService: MediaService,
+  ) {}
+
+  ngAfterViewInit() {
+    this.scrollContainer = this.scrollFrame.nativeElement
+    this.itemElements.changes.subscribe((_) => this.onItemElementsChanged())
+  }
+  private onItemElementsChanged(): void {
+    this.scrollToBottom()
+  }
+  private scrollToBottom(): void {
+    this.scrollContainer.scroll({
+      top: this.scrollContainer.scrollHeight,
+      left: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  get question() {
+    return this.chatForm.get('question')
+  }
+
+  ngOnInit() {
+    this.service.init()
+  }
+
+  newChat() {
+    this.service.newChat()
+  }
+  switchActiveChat(id: number) {
+    this.service.switchActiveChat(id)
+  }
+
+  onAsk(question: string | null | undefined) {
+    if (this.chatForm.valid && question) {
+      this.chatForm.reset()
+      this.service.ask(question).pipe(untilDestroyed(this)).subscribe()
+    }
+  }
+
+  openHistory(content: TemplateRef<any>): void {
+    this.offcanvasService.open(content, {
+      ariaLabelledBy: 'offcanvas-basic-title',
+    })
+  }
+}
