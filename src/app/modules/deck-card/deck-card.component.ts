d95c6baa53fc94b5bc522d9be17ad00062adf70e
Zavierazo <zavierazo@gmail.com>
2025-05-09 19:28:25 +0200

    Refactor deck card component layout and styles for improved responsiveness and aesthetics

--- src/app/modules/deck-card/deck-card.component.ts
+++ src/app/modules/deck-card/deck-card.component.ts
@@ -1,21 +1,30 @@
-import { ApiDeck } from '../../models/api-deck'
+import { NgClass, NgStyle, TitleCasePipe } from '@angular/common'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
-import { MediaService } from '../../services/media.service'
+import { RouterLink } from '@angular/router'
+import { TranslocoPipe } from '@jsverse/transloco'
+import { TranslocoDatePipe } from '@jsverse/transloco-locale'
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
 import { tap } from 'rxjs'
-import { RouterLink } from '@angular/router';
-import { NgClass, NgStyle, NgIf, NgFor, TitleCasePipe } from '@angular/common';
-import { TruncatePipe } from '../../shared/pipes/truncate.pipe';
-import { TranslocoFallbackPipe } from '../../shared/pipes/transloco-fallback';
-import { TranslocoPipe } from '@jsverse/transloco';
-import { TranslocoDatePipe } from '@jsverse/transloco-locale';
+import { ApiDeck } from '../../models/api-deck'
+import { MediaService } from '../../services/media.service'
+import { TranslocoFallbackPipe } from '../../shared/pipes/transloco-fallback'
+import { TruncatePipe } from '../../shared/pipes/truncate.pipe'
 
 @UntilDestroy()
 @Component({
-    selector: 'app-deck-card',
-    templateUrl: './deck-card.component.html',
-    styleUrls: ['./deck-card.component.scss'],
-    imports: [RouterLink, NgClass, NgStyle, NgIf, NgFor, TitleCasePipe, TruncatePipe, TranslocoFallbackPipe, TranslocoPipe, TranslocoDatePipe]
+  selector: 'app-deck-card',
+  templateUrl: './deck-card.component.html',
+  styleUrls: ['./deck-card.component.scss'],
+  imports: [
+    RouterLink,
+    NgClass,
+    NgStyle,
+    TitleCasePipe,
+    TruncatePipe,
+    TranslocoFallbackPipe,
+    TranslocoPipe,
+    TranslocoDatePipe,
+  ],
 })
 export class DeckCardComponent implements OnInit {
   @Input() deck!: ApiDeck
