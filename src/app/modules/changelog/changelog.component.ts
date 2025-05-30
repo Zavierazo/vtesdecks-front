6b510d6be3185a1e7188da99c700cc1ff9570768
Zavierazo <zavierazo@gmail.com>
2025-05-30 18:11:56 +0200

    Migrate angular ngFor/ngIf to @for/@if

--- src/app/modules/changelog/changelog.component.ts
+++ src/app/modules/changelog/changelog.component.ts
@@ -1,20 +1,20 @@
+import { AsyncPipe } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
+import { TranslocoDirective } from '@jsverse/transloco'
 import { Observable } from 'rxjs'
 import { ApiChangelog } from '../../models/api-changelog'
 import { ApiDataService } from './../../services/api.data.service'
-import { TranslocoDirective } from '@jsverse/transloco';
-import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-changelog',
-    templateUrl: './changelog.component.html',
-    styleUrls: ['./changelog.component.scss'],
-    imports: [TranslocoDirective, NgIf, NgFor, AsyncPipe]
+  selector: 'app-changelog',
+  templateUrl: './changelog.component.html',
+  styleUrls: ['./changelog.component.scss'],
+  imports: [TranslocoDirective, AsyncPipe],
 })
 export class ChangelogComponent implements OnInit {
   changelog$!: Observable<ApiChangelog[]>
 
-  constructor(private apiDataService: ApiDataService) {}
+  constructor(private readonly apiDataService: ApiDataService) {}
 
   ngOnInit() {
     this.changelog$ = this.apiDataService.getChangelog()
