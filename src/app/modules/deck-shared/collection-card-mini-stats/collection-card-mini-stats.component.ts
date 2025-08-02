b8f1bee4818d02c8fc49a409d76de45c69eb20f5
Zavierazo <zavierazo@gmail.com>
2025-08-02 19:20:46 +0200

    feat(collection): update collection card mini stats to use ApiCard and improve styling based on collection stats

--- src/app/modules/deck-shared/collection-card-mini-stats/collection-card-mini-stats.component.ts
+++ src/app/modules/deck-shared/collection-card-mini-stats/collection-card-mini-stats.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core'
 import { TranslocoDirective } from '@jsverse/transloco'
 import { EMPTY } from 'rxjs'
+import { ApiCard } from '../../../models/api-card'
 import { ApiDataService } from '../../../services/api.data.service'
 import { AuthQuery } from '../../../state/auth/auth.query'
 
@@ -23,12 +24,13 @@ export class CollectionCardMiniStatsComponent {
   private readonly authQuery = inject(AuthQuery)
   private readonly apiDataService = inject(ApiDataService)
 
-  cardId = input.required<number>()
+  card = input.required<ApiCard>()
+
   routerClick = output<boolean>()
 
   collectionStats$ = computed(() => {
     if (this.authQuery.isAuthenticated()) {
-      return this.apiDataService.getCardCollectionStats(this.cardId(), true)
+      return this.apiDataService.getCardCollectionStats(this.card().id, true)
     }
     return EMPTY
   })
