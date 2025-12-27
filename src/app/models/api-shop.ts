c999412c46decb69824d4ae45df55268751889e4
Zavierazo <zavierazo@gmail.com>
2025-12-27 15:03:03 +0100

    feat: add locale support to shop info and implement displayLocale pipe

--- src/app/models/api-shop.ts
+++ src/app/models/api-shop.ts
@@ -3,7 +3,8 @@ import { ApiShopInfo } from './api-shop-info'
 export interface ApiShop {
   cardId: number
   shopInfo: ApiShopInfo
-  set: string
+  set?: string
+  locale?: string
   link: string
   price: number
   currency: string
