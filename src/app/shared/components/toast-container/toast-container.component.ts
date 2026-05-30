d676d7cb5eb7f52968f2fd7e1aa6c8cd67100257
Zavierazo <zavierazo@gmail.com>
2026-05-30 11:11:02 +0200

    fix: adjust z-index for toast container to prevent overlap issues

--- src/app/shared/components/toast-container/toast-container.component.ts
+++ src/app/shared/components/toast-container/toast-container.component.ts
@@ -23,7 +23,7 @@ import { ToastService } from '@services'
   `,
   host: {
     class: 'toast-container position-fixed top-0 end-0 p-3',
-    style: 'z-index: 1200; margin-top: 4rem;',
+    style: 'z-index: 999; margin-top: 4rem;',
   },
   imports: [NgbToast, NgTemplateOutlet],
 })
