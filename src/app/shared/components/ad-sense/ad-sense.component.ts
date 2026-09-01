fbc36c9d53dcbb02dc066a8e97599a3453582769
Zavierazo <zavierazo@gmail.com>
2026-09-01 15:01:30 +0200

    feat: improve patron benefits visibility

--- src/app/shared/components/ad-sense/ad-sense.component.ts
+++ src/app/shared/components/ad-sense/ad-sense.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { TranslocoPipe } from '@jsverse/transloco'
 import { AdSenseDirective } from '../../directives/ad-sense.directive'
+import { IsSupporterDirective } from '../../directives/is-supporter.directive'
 
 @Component({
   selector: 'app-ad-sense',
   templateUrl: './ad-sense.component.html',
   styleUrls: ['./ad-sense.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AdSenseDirective],
+  imports: [AdSenseDirective, IsSupporterDirective, TranslocoPipe],
 })
 export class AdSenseComponent {
   @Input() adClient!: string
